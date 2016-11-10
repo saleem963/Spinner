863d2694ba4fb15efb60dd2126cfa6a728917291
saleem <saleem.in963@gmail.com>
2016-11-10 10:11:10 +0530

    change

--- app/src/main/java/com/example/prabodhananda/spinner/MainActivity.java
+++ app/src/main/java/com/example/prabodhananda/spinner/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    // names
     Spinner spp;
     String names [] = {"sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
     @Override
